244d6f04883a7a262ce1262c4576b2a6618a2273
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-03-31 13:51:41 +0200

    Typography component | props: color, variant, component, gutterBottom

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,16 @@
+import Typography from '@material-ui/core/Typography';
+
 const Create = () => {
     return ( 
         <div className="Create">
-            Create notes
+            <Typography
+                variant="h6" //still looks like an h6
+                color="textSecondary"
+                component="h2" //but I actually want it to be an h2 element
+                gutterBottom
+            >
+                Create a New Note
+            </Typography>
         </div>
      );
 }
