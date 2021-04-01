02e573b5591f9c8f99da25fe1df33893a4620a34
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-04-01 03:07:16 +0200

    installed material-ui icons and explored displaying icons in buttons

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,8 +1,12 @@
 import Typography from '@material-ui/core/Typography';
-
+import Button from '@material-ui/core/Button';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Container from '@material-ui/core/Container';
+import SendIcon from '@material-ui/icons/Send';
+import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 const Create = () => {
     return ( 
-        <div className="Create">
+        <Container className="Create">
             <Typography
                 variant="h6" //still looks like an h6
                 color="textSecondary"
@@ -11,7 +15,35 @@ const Create = () => {
             >
                 Create a New Note
             </Typography>
-        </div>
+
+            <Button
+                onClick={() => console.log('you clicked me')}
+                type="submit"
+                color="secondary"
+                variant="contained"
+                endIcon={<KeyboardArrowRightIcon />}
+            >
+                Submit
+            </Button>
+            {/* <Button type="submit">Submit</Button>
+            <Button type="submit" color="secondary" variant="outlined">Submit</Button>
+
+            <ButtonGroup color="secondary" variant="contained">
+                <Button>One</Button>
+                <Button>Two</Button>
+                <Button>Three</Button>
+                <Button>Four</Button>
+
+            </ButtonGroup> */}
+            {/* icons */}
+            {/* <br/>
+            <AcUnitOutlinedIcon color="secondary" fontSize="small" />
+            <AcUnitOutlinedIcon color="primary" fontSize="large" />
+            <AcUnitOutlinedIcon color="error" fontSize="large" />
+            <AcUnitOutlinedIcon color="disabled" fontSize="large" />
+            <AcUnitOutlinedIcon color="action" fontSize="large" /> */}
+
+        </Container>
      );
 }
  
